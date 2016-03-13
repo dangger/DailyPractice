f67d9f2c5e83f0f5b02eb4e31942477d298c822e
dangger <dangge2@gmail.com>
2016-03-13 14:44:30 +0800

    2016年3月13日联系

---
+++ 2016_03/AandB.java
@@ -0,0 +1,13 @@
+/**
+ * Created by dangge on 2016/3/13.
+ */
+import java.io.*;
+import java.util.Scanner;
+public class AandB {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
